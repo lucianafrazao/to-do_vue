e9f53e467ff6a68d9035300de89009b4b3f09b1f
Lu Frazão <lugfrazao@gmail.com>
2025-01-07 21:36:58 -0300

    creating todo-list in one file

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,94 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+  const state = reactive({
+    filtering: 'all',
+    tempTask: '',
+    tasks: [
+      {
+        title: 'Study EBAC',
+        done: false,
+      },
+      {
+        title: 'Go to the gym',
+        done: true,
+      },
+      {
+        title: 'Take car to the shop',
+        done: true,
+      }
+    ]
+  })
+
+  const getUnfinishedTasks = () => {
+    return state.tasks.filter(task => !task.done)
+  }
+
+  const getFinishedTasks = () => {
+    return state.tasks.filter(task => task.done)
+  }
+
+  const getFilteredTasks = () => {
+    const { filtering } = state;
+
+    switch (filtering) {
+      case 'unfinished':
+        return getUnfinishedTasks();
+      case 'finished':
+        return getFinishedTasks();
+      default:
+        return state.tasks;
+    }
+  }
+
+  const addTask = () => {
+    const newTask = {
+      title: state.tempTask,
+      done: false,
+    }
+    state.tasks.push(newTask);
+    state.tempTask = '';
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="p-5 mb-4 bg-light rounded-3">
+      <h1>My to do list</h1>
+      <p>
+        You have {{ getUnfinishedTasks().length }} unfinished task(s).
+      </p>
+    </header>
+    <form @submit.prevent="addTask">
+      <div class="row">
+        <div class="col">
+          <input :value="state.tempTask" @change="event => state.tempTask = event.target.value" required type="text" placeholder="Task description" class="form-control"></div>
+          <div class="col-md-1">
+            <button type="submit" class="btn btn-primary">Add</button>
+          </div>
+          <div class="col-md-2">
+            <select @change="event => state.filtering = event.target.value" class="form-control">
+              <option value="all">All tasks</option>
+              <option value="unfinished">Unfinished tasks</option>
+              <option value="finished">Finished tasks</option>
+              <p></p>
+            </select>
+          </div>
+      </div>
+    </form>
+    <ul class="list-group mt-4">
+      <li class="list-group-item" v-for="task in getFilteredTasks()">
+        <input @change="event => task.done = event.target.checked" :checked="task.done" :id="task.title" type="checkbox">
+        <label :class="{ taskDone: task.done}" class="ms-3">
+          {{ task.title }}
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .taskDone {
+    text-decoration: line-through;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
